bf111473aa1289a654fe0787573ccc33abd04233
Martin Diaz Preiero <martinpreierodiaz@gmail.com>
2024-08-03 04:21:33 -0300

    chore: Remove unused database connection code

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import db from "./config/mongo";
 import adminRoutes from "./routes/admin.route";
 import bookRoutes from "./routes/book.route";
 
@@ -11,12 +10,4 @@ app.use(express.json())
 app.use(cors())
 app.use(adminRoutes);
 app.use(bookRoutes);
-db().then(() => {
-    console.log('Database connected successfully');
-}).catch((err) => {
-    console.error('Database connection error:', err);
-    process.exit(1);
-});
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+console.log('Database connected successfully');
